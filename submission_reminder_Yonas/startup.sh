7e1a419fe7f858f49488d425e86780f596275c22
Noella799 <n.uwera@alustudent.com>
2025-06-19 12:05:39 -0700

    merged and deleted unnecessary files

--- submission_reminder_Yonas/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#load environment variable from the config file
-source ./config/config.env
-#load addition functions from another file
-source ./modules/functions.sh
-#execute the reminder
-bash ./app/reminder.sh
